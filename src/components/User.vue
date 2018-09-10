0773fb3450d546dad38d025457d34cc56bd193a4
Santosh Marigowda <santosharakere@gmail.com>
2018-09-10 22:18:16 +0100

    passing props from parent to child

--- src/components/User.vue
+++ src/components/User.vue
@@ -2,10 +2,11 @@
     <div class="component parent">
         <h1>The User Component</h1>
         <p>I'm an awesome User!</p>
+        <button @click="changeName">Change Name</button>
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-6">
-                <app-user-detail></app-user-detail>
+                <app-user-detail :name="name"></app-user-detail>
             </div>
             <div class="col-xs-12 col-sm-6">
                 <app-user-edit></app-user-edit>
@@ -19,6 +20,16 @@
     import UserEdit from './UserEdit.vue';
 
     export default {
+        data: function() {
+          return {
+            name: 'Santosh'
+          }
+        },
+        methods: {
+          changeName() {
+            this.name = 'Roopa'
+          }
+        },
         components: {
             appUserDetail: UserDetail,
             appUserEdit: UserEdit
--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -2,10 +2,14 @@
     <div class="component child1">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
+        <p>Name: {{ name }}</p>
     </div>
 </template>
 
 <script>
+  export default {
+    props: ['name']
+  }
 </script>
 
 <style scope>
