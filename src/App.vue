603971b9646440175bfcd97aaaa3a608f058a406
Santosh Marigowda <santosharakere@gmail.com>
2018-09-18 21:40:29 +0100

    dynamic components with default content

--- src/App.vue
+++ src/App.vue
@@ -2,25 +2,38 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-               <app-quote>
+               <button @click="selectedComponent = 'appQuote'">Qoute</button>
+               <button @click="selectedComponent = 'appAuthor'">Author</button>
+               <button @click="selectedComponent = 'appNew'">New</button>
+               <hr>
+               <p>{{ selectedComponent }}</p>
+               <component :is="selectedComponent">
+                   <div>Default Component</div>
+               </component>
+               <!-- <app-quote>
                    <h1 slot="title">{{ quoteTitle }}</h1>
                    <p>A wonderful Quote</p>
-               </app-quote>
+               </app-quote> -->
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import Quote from './components/Quote.vue'
+    import Quote from './components/Quote.vue';
+    import Author from './components/Author.vue';
+    import New from './components/New.vue'
     export default {
         data: function() {
             return {
-                quoteTitle: 'The Quote !'
+                quoteTitle: 'The Quote !',
+                selectedComponent: 'appQuote'
             }
         },
         components: {
-            appQuote: Quote
+            appQuote: Quote,
+            appAuthor: Author,
+            appNew: New
         }
     }
 </script>
---
+++ src/components/New.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h3>New Component</h3>
+    <p>New Quote</p>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+</style>
+
