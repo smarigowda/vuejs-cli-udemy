64cdc5bd858b06ba9c5edbb04eb43ff2533c5861
Santosh Marigowda <santosharakere@gmail.com>
2018-09-19 06:50:26 +0100

    passing content using method

--- src/App.vue
+++ src/App.vue
@@ -2,47 +2,49 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-               <button @click="selectedComponent = 'appQuote'">Qoute</button>
-               <button @click="selectedComponent = 'appAuthor'">Author</button>
-               <button @click="selectedComponent = 'appNew'">New</button>
-               <hr>
-               <p>{{ selectedComponent }}</p>
-               <keep-alive>
+                <br>
+                <button class="btn btn-primary" @click="selectAComponent('app-blue', 'Blue Rectangle...')">Load Blue Template</button>
+                <button class="btn btn-success" @click="selectedComponent = 'app-green'">Load Green Template</button>
+                <button class="btn btn-danger" @click="selectedComponent = 'app-red'">Load Red Template</button>
+                <hr>
+                <div v-if="selectedComponent">
                     <component :is="selectedComponent">
-                        <div>Default Component</div>
+                        <template>{{ content }}</template>
                     </component>
-               </keep-alive>
-
-               <!-- <app-quote>
-                   <h1 slot="title">{{ quoteTitle }}</h1>
-                   <p>A wonderful Quote</p>
-               </app-quote> -->
+                </div>
+                <!-- <app-blue></app-blue>
+                <app-green></app-green>
+                <app-red></app-red> -->
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import Quote from './components/Quote.vue';
-    import Author from './components/Author.vue';
-    import New from './components/New.vue'
+    import Blue from './components/Blue.vue';
+    import Green from './components/Green.vue';
+    import Red from './components/Red.vue';
+
     export default {
         data: function() {
             return {
-                quoteTitle: 'The Quote !',
-                selectedComponent: 'appQuote'
+                selectedComponent: null,
+                content: null
             }
         },
         components: {
-            appQuote: Quote,
-            appAuthor: Author,
-            appNew: New
+            appBlue: Blue,
+            appGreen: Green,
+            appRed: Red
+        },
+        methods: {
+            selectAComponent(component, content) {
+                this.selectedComponent = component;
+                this.content = content;
+            }
         }
     }
 </script>
 
-<style scoped>
-    hr {
-        border-top: 1px solid black;
-    }
+<style>
 </style>
