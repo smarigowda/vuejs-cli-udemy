4f9e758c0a2bdc0f0e221b017ef63b1ff9cdc5bd
Santosh Marigowda <santosharakere@gmail.com>
2018-09-19 05:28:13 +0100

    dynamic component, destroyed lifecycle method

--- src/components/New.vue
+++ src/components/New.vue
@@ -2,11 +2,21 @@
   <div>
     <h3>New Component</h3>
     <p>New Quote</p>
+    <button @click="counter++">INCREMENT</button>
+    <p>{{ counter }}</p>
   </div>
 </template>
 
 <script>
 export default {
+  data: function() {
+    return {
+      counter: 0
+    }
+  },
+  destroyed() {
+    console.log('New Component: Destroyed !')
+  }
 }
 </script>
 
