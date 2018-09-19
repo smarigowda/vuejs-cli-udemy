e4895904e52a1a2ce0f0b638982a147d6c5e6b53
Santosh <santosharakere@gmail.com>
2018-09-19 22:01:22 +0100

    section 10 startup code

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,14 @@
 <template>
     <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-               <button @click="selectedComponent = 'appQuote'">Qoute</button>
-               <button @click="selectedComponent = 'appAuthor'">Author</button>
-               <button @click="selectedComponent = 'appNew'">New</button>
-               <hr>
-               <p>{{ selectedComponent }}</p>
-               <keep-alive>
-                    <component :is="selectedComponent">
-                        <div>Default Component</div>
-                    </component>
-               </keep-alive>
-
-               <!-- <app-quote>
-                   <h1 slot="title">{{ quoteTitle }}</h1>
-                   <p>A wonderful Quote</p>
-               </app-quote> -->
-            </div>
-        </div>
+    
     </div>
 </template>
 
 <script>
-    import Quote from './components/Quote.vue';
-    import Author from './components/Author.vue';
-    import New from './components/New.vue'
     export default {
-        data: function() {
-            return {
-                quoteTitle: 'The Quote !',
-                selectedComponent: 'appQuote'
-            }
-        },
-        components: {
-            appQuote: Quote,
-            appAuthor: Author,
-            appNew: New
-        }
+        
     }
 </script>
 
-<style scoped>
-    hr {
-        border-top: 1px solid black;
-    }
+<style>
 </style>
