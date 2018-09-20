123fcc165f9eff7065ddc8e167e33434046d7b09
Santosh Marigowda <santosharakere@gmail.com>
2018-09-20 23:46:29 +0100

    single quote displayed

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <template>
     <div class="container">
-    
+        <app-quote-grid :quotes="quotes"></app-quote-grid>
     </div>
 </template>
 
 <script>
+    import QuoteGrid from './components/QuoteGrid.vue';
     export default {
-        
+        data: function() {
+            return {
+                quotes: [
+                    'Just a Quote...'
+                ],
+                maxQuotes: 10
+            }
+        },
+        components: {
+            appQuoteGrid: QuoteGrid
+        }
     }
 </script>
 
