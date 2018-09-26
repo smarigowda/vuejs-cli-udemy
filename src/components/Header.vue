65c6036a050775ad8b98fa85c959b0237c8b7425
Santosh Marigowda <santosharakere@gmail.com>
2018-09-26 22:03:00 +0100

    progress bar html code

---
+++ src/components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h3>Quotes Added</h3>
+    <div class="progress">
+      <div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100">
+        
+      </div>
+    </div>
+  </div>
+</template>
