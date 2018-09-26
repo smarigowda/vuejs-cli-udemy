e50a3ae7c5d662459b0170ad23318293ec5a5d1b
Santosh Marigowda <santosharakere@gmail.com>
2018-09-26 05:12:35 +0100

    adding a NewQuote component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
     <div class="container">
+        <app-new-quote></app-new-quote>
         <app-quote-grid :quotes="quotes"></app-quote-grid>
     </div>
 </template>
 
 <script>
     import QuoteGrid from './components/QuoteGrid.vue';
+    import NewQuote from './components/NewQuote.vue';
+
     export default {
         data: function() {
             return {
@@ -16,7 +19,8 @@
             }
         },
         components: {
-            appQuoteGrid: QuoteGrid
+            appQuoteGrid: QuoteGrid,
+            appNewQuote: NewQuote,
         }
     }
 </script>
