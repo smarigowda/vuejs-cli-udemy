de9157b4b3d10cd5d4a8e18a447c745f57f81f4b
Santosh Marigowda <santosharakere@gmail.com>
2018-09-30 16:33:47 +0100

    using modifiers with v-model

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@
                                 type="password"
                                 id="password"
                                 class="form-control"
-                                v-model="userData.password">
+                                v-model.lazy="userData.password">
                     </div>
                     <div class="form-group">
                         <label for="age">Age</label>
@@ -27,7 +27,7 @@
                                 type="number"
                                 id="age"
                                 class="form-control"
-                                v-model="userData.age">
+                                v-model.number="userData.age">
                     </div>
 
                 </div>
