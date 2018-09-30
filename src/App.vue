cfd92ee2baacb22d37304074e07677bf79e83a26
Santosh Marigowda <santosharakere@gmail.com>
2018-09-30 21:28:10 +0100

    binding array to checkbox inputs

--- src/App.vue
+++ src/App.vue
@@ -50,13 +50,15 @@
                             <input
                                     type="checkbox"
                                     id="sendmail"
-                                    value="SendMail"> Send Mail
+                                    value="SendMail"
+                                    v-model="sendMail"> Send Mail
                         </label>
                         <label for="sendInfomail">
                             <input
                                     type="checkbox"
                                     id="sendInfomail"
-                                    value="SendInfoMail"> Send Infomail
+                                    value="SendInfoMail"
+                                    v-model="sendMail"> Send Infomail
                         </label>
                     </div>
 
@@ -111,7 +113,7 @@
                         <p style="white-space: pre">Message: {{ message }}</p>
                         <p><strong>Send Mail?</strong></p>
                         <ul>
-                            <li></li>
+                            <li v-for="item in sendMail">{{ item }}</li>
                         </ul>
                         <p>Gender:</p>
                         <p>Priority:</p>
@@ -133,6 +135,7 @@
                     age: 27,
                 },
                 message: 'A text message',
+                sendMail: [],
             }
         }
     }
