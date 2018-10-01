961e113f3b143bcbeb9e9352d5ded3761c3852f2
Santosh Marigowda <santosharakere@gmail.com>
2018-10-01 21:54:51 +0100

    custom switch complete

--- src/App.vue
+++ src/App.vue
@@ -94,6 +94,11 @@
                     </select>
                 </div>
             </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 from-group">
+                    <app-switch v-model="dataSwitch"></app-switch>
+                </div>
+            </div>
             <hr>
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
@@ -121,7 +126,7 @@
                         </ul>
                         <p>Gender: {{ gender }}</p>
                         <p>Priority: {{ selectedPriority }}</p>
-                        <p>Switched:</p>
+                        <p>Switched: {{ dataSwitch }}</p>
                     </div>
                 </div>
             </div>
@@ -130,6 +135,8 @@
 </template>
 
 <script>
+    import Switch from './Switch.vue';
+
     export default {
         data() {
             return {
@@ -142,8 +149,12 @@
                 sendMail: [],
                 gender: 'Male',
                 selectedPriority: 'High',
-                priority: ['High', 'Medium', 'Low']
+                priority: ['High', 'Medium', 'Low'],
+                dataSwitch: true,
             }
+        },
+        components: {
+            appSwitch: Switch,
         }
     }
 </script>
