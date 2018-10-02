bd71ef22f1dd208457dce9e6dac081837dfc5788
Santosh Marigowda <santosharakere@gmail.com>
2018-10-02 19:59:58 +0100

    built in directives

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Directives</h1>
+                <h1>Built in Directives</h1>
+                <p v-text="'Some Text...'"></p>
+                <p v-html="'<strong>Some Strong Text...</strong>'"></p>
             </div>
         </div>
     </div>
