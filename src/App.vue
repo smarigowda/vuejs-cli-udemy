51132daaaec7a6be0817849ea0c6bd4582770233
Santosh Marigowda <santosharakere@gmail.com>
2018-10-02 21:30:46 +0100

    passing value

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Custom Directives</h1>
-                <p v-highlight>Color This Paragraph</p>
+                <p v-highlight="'red'">Color This Paragraph</p>
             </div>
         </div>        
     </div>
