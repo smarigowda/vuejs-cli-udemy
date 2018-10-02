3eadd2018fdb323f34c2d2975a45992193d160b8
Santosh Marigowda <santosharakere@gmail.com>
2018-10-02 05:16:34 +0100

    form submission complete

--- src/App.vue
+++ src/App.vue
@@ -103,13 +103,14 @@
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                     <button
-                            class="btn btn-primary">Submit!
+                            class="btn btn-primary"
+                            @click.prevent="submitted">Submit!
                     </button>
                 </div>
             </div>
         </form>
         <hr>
-        <div class="row">
+        <div class="row" v-if="isFormSubmitted">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
                     <div class="panel-heading">
@@ -151,10 +152,16 @@
                 selectedPriority: 'High',
                 priority: ['High', 'Medium', 'Low'],
                 dataSwitch: true,
+                isFormSubmitted: false,
             }
         },
         components: {
             appSwitch: Switch,
+        },
+        methods: {
+            submitted() {
+                this.isFormSubmitted = true;
+            }
         }
     }
 </script>
