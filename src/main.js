9b11d9e294815a3b824422b104d403dc6f0b1d9f
Santosh Marigowda <santosharakere@gmail.com>
2018-10-02 21:24:55 +0100

    a simple custom directive

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
 
+Vue.directive('highlight', {
+  bind(el, binding, vnode) {
+    el.style.backgroundColor = 'green'
+  }
+})
 export const eventBus = new Vue();
 
 new Vue({
