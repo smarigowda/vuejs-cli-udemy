b38339023d16784347464ed5f478da7b693520da
Santosh Marigowda <santosharakere@gmail.com>
2018-10-04 05:46:56 +0100

    custom v-on like directive

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,8 @@
   <div class="container">
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <h1>Built in Directives</h1>
-        <p v-text="'Some Text...'"></p>
-        <p v-html="'<strong>Some Strong Text...</strong>'"></p>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <h1>Custom Directives</h1>
-        <p v-highlight:background.delayed="'red'">Color This Paragraph</p>
-        <p v-local-highlight:background.delayed.blink="{ mainColor: 'red', secondColor: 'green', delay: 500 }">Color This Paragraph</p>
+        <h1>Custom Directive Assignment</h1>
+        <button v-customOn="clicked" class="btn btn-primary">Click Me !</button>
       </div>
     </div>
   </div>
@@ -20,38 +12,17 @@
 <script>
 export default {
   directives: {
-    "local-highlight": {
-      bind(el, binding, vnode) {
-        let delay = 0;
-        if (binding.modifiers["delayed"]) {
-          delay = 3000;
-        }
-
-        if (binding.modifiers["blink"]) {
-          let mainColor = binding.value.mainColor;
-          let secondColor = binding.value.secondColor;
-          let currentColor = mainColor;
-          setTimeout(() => {
-            setInterval(() => {
-              currentColor === secondColor ? currentColor = mainColor : currentColor = secondColor;
-              if (binding.arg === "background") {
-                el.style.backgroundColor = currentColor;
-              } else {
-                el.style.color = currentColor;
-              }
-            }, binding.value.delay)
-
-          }, delay);
-        } else {
-          setTimeout(() => {
-            if (binding.arg === "background") {
-              el.style.backgroundColor = binding.value.mainColor;
-            } else {
-              el.style.color = binding.value.mainColor;
-            }
-          }, delay);
+    customOn: {
+      bind(el, binding) {
+        el.onclick = function() {
+          binding.value();
         }
       }
+    }
+  },
+  methods: {
+    clicked() {
+      alert('clicked...')
     }
   }
 };
