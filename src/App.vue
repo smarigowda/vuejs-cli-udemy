a127adba19dc373a341883458806e4ba80156dfc
Santosh Marigowda <santosharakere@gmail.com>
2018-10-04 04:55:32 +0100

    using multiple modifiers

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Built in Directives</h1>
-                <p v-text="'Some Text...'"></p>
-                <p v-html="'<strong>Some Strong Text...</strong>'"></p>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Custom Directives</h1>
-                <p v-highlight:background.delayed="'red'">Color This Paragraph</p>
-                <p v-local-highlight:background.delayed="'cyan'">Color This Paragraph</p>
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Built in Directives</h1>
+        <p v-text="'Some Text...'"></p>
+        <p v-html="'<strong>Some Strong Text...</strong>'"></p>
+      </div>
     </div>
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Custom Directives</h1>
+        <p v-highlight:background.delayed="'red'">Color This Paragraph</p>
+        <p v-local-highlight:background.delayed.blink="'cyan'">Color This Paragraph</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -27,13 +27,30 @@ export default {
           delay = 3000;
         }
 
-        setTimeout(() => {
-          if (binding.arg === "background") {
-            el.style.backgroundColor = binding.value;
-          } else {
-            el.style.color = binding.value;
-          }
-        }, delay);
+        if (binding.modifiers["blink"]) {
+          let mainColor = binding.value;
+          let secondColor = "blue";
+          let currentColor = mainColor;
+          setTimeout(() => {
+            setInterval(() => {
+              currentColor === secondColor ? currentColor = mainColor : currentColor = secondColor;
+              if (binding.arg === "background") {
+                el.style.backgroundColor = currentColor;
+              } else {
+                el.style.color = currentColor;
+              }
+            }, 1000)
+
+          }, delay);
+        } else {
+          setTimeout(() => {
+            if (binding.arg === "background") {
+              el.style.backgroundColor = binding.value;
+            } else {
+              el.style.color = binding.value;
+            }
+          }, delay);
+        }
       }
     }
   }
