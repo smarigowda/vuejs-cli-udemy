0ccb258a290a9f47627114e5c4232a10e5065eca
Santosh Marigowda <santosharakere@gmail.com>
2018-10-05 05:25:36 +0100

    duplicate code

--- src/List.vue
+++ src/List.vue
@@ -1,23 +1,14 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <div>
               <h1>Filters & Mixins</h1>
-              <p>{{ text | toUppercase | toLowerCase }}</p>
-              <hr/>
               <input type="text" v-model="filterText"/>
               <ul>
                 <li v-for="fruit in filteredFruits">{{ fruit }}</li>
               </ul>
-              <hr>
-              <app-list></app-list>
-            </div>
         </div>
-    </div>
 </template>
 
 <script>
-    import List from './List.vue'
     export default {
       data() {
         return {
@@ -37,9 +28,6 @@
             return d.match(this.filterText);
           })
         }
-      },
-      components: {
-        appList: List
       }
     }
 </script>
