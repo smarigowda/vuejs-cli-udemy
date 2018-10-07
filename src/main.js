2cd5c1e01a074b96a3e0f4ce711bdad0c649dbe5
Santosh Marigowda <santosharakere@gmail.com>
2018-10-07 10:31:29 +0100

    global mixin

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
 
+Vue.mixin({
+  created() {
+    console.log('[Global Mixin] created life cycle hook');
+  }
+});
+
 Vue.directive('highlight', {
   bind(el, binding, vnode) {
     // el.style.backgroundColor = 'green'
